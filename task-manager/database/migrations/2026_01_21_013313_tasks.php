8481d01792749c000b770fa4a9fe30e17ba4b3e2
Jumpy85Plays <gabriel42498@gmail.com>
2026-01-21 14:02:49 +0800

    slight progress in models

--- task-manager/database/migrations/2026_01_21_013313_tasks.php
+++ task-manager/database/migrations/2026_01_21_013313_tasks.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id(); // Primary key column called id that auto-increments
             $table->string('title'); // String column for the task title
             $table->text('description')->nullable(); // Text column for task description, can be null
-            $table->date('due_date')->nullable(); // Date column for due date, can be null
             $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending'); // Enum column for task status with default value
             $table->timestamps(); // Timestamp columns created_at and updated_at
 
